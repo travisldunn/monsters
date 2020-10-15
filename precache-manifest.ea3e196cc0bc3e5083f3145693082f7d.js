3b06c5f49014aa7f519c2f4c1864282a7d5846ba
Travis Dunn <Travisldunn@gmail.com>
2020-10-15 10:41:14 -0400

    Updates

---
+++ precache-manifest.ea3e196cc0bc3e5083f3145693082f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e8c64e6c8d4759e9b7d41292c56537",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c8e1716b21e2bc1e041e",
+    "url": "/monsters/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e1716b21e2bc1e041e",
+    "url": "/monsters/static/js/main.61eb10f5.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
